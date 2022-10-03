a3f490dbdb8d5985eae861cf62e578277ee2c7b8
cb11711211 <12133069@mail.sustech.edu.cn>
2022-10-03 20:36:35 +0800

    module system

--- node_js_design_pattern/myModule.js
+++ node_js_design_pattern/myModule.js
@@ -20,3 +20,76 @@ module.exports = () => {
     console.log('Hello World')
 }
 
+// 2.5 Using the module
+// named exports
+// file logger.js
+exports.info = (msg) => {
+    console.log(`Info: ${msg}`)
+}
+exports.verbose = (msg) => {
+    console.log(`Verbose: ${msg}`)
+}
+
+// file main.js
+const logger = require('./logger')
+logger.info('This is an info message')
+logger.verbose('This is a verbose message')
+
+
+// exporting function
+// file logger.js
+module.exports = (msg) => {
+    console.log(`Info: ${msg}`)
+}
+
+// alternatively
+module.exports.verbose = (msg) => {
+    console.log(`Verbose: ${msg}`)
+}
+
+// file main.js
+const logger = require('./logger')
+logger('This is an info message')
+logger.verbose('This is a verbose message')
+
+
+// exporting class
+class Logger {
+    constructor (name) {
+        this.name = name
+    }
+    log(msg) {
+        console.log(`[${this.name}] ${msg}`)
+    }
+    info(msg) {
+        console.log(`Info: ${msg}`)
+    }
+    verbose(msg) {
+        console.log(`Verbose: ${msg}`)
+    }
+}
+//file main.js
+const Logger = require('./logger')
+const dblogger = new Logger('db')
+dblogger.log('This is a log message')
+const accesslogger = new Logger('access')
+accesslogger.verbose('This is a verbose message')
+
+
+// exporting instance
+// file logger.js
+class Logger {
+    constructor(name) {
+        this.count = 0
+        this.name = name
+    }
+    log(msg) {
+        this.count++
+        console.log(`[${this.name}] ${msg}`)
+    }
+}
+module.exports = new Logger('default')
+
+// file main.js
+const logger = require('./logger')
+logger.log('This is a log message')
